b08fcaae5137f07c47d1cdadcd2229f1230c82f1
Renzi_lean <renzir2@gmail.com>
2026-02-10 14:39:47 +0100

    Fix: Update product.get.service.js

--- features/products/product.get.service.js
+++ features/products/product.get.service.js
@@ -1,7 +1,7 @@
 const db = require("../../db/database");
 
 async function getProducts() {
-  const [products] = await db.execute("select * from tienda.productos");
+  const [products] = await db.execute("select * from railway.productos");
   return products || null;
 }
 module.exports = getProducts;
