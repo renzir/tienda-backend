6d5b323134428374aae6b5874131a1de2a8e5a20
Renzi_lean <renzir2@gmail.com>
2026-02-11 13:53:12 +0100

    fix: enable CORS for frontend Netlify

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors"); // <-- agregá esto
 const routerProducts = require("./features/products/product.router.js");
 const MiddlewareErrores = require("./middleware/MiddlewareErrores.js");
 const routerOrder = require("./features/orders/order.router.js");
@@ -8,10 +9,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+app.use(cors({
+  origin: "https://tienda-frontend-portafolio.netlify.app", 
+  methods: ["GET", "POST", "PUT", "DELETE"], 
+  credentials: true 
+}));
+
+
 app.use(express.json());
 
 app.use("/products", routerProducts);
-
 app.use("/order", routerOrder);
 
 app.use(MiddlewareErrores);
